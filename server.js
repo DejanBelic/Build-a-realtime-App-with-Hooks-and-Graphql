65b3dfa85ca24043e6ce7feda712583252391b6f
Dejan <dejan.belic@develomon.com>
2019-04-14 11:17:12 +0200

    Creating database with mongodb atlas, l4.

--- server.js
+++ server.js
@@ -2,6 +2,14 @@ const { ApolloServer } = require('apollo-server');
 
 const typeDefs = require('./typeDefs.js');
 const resolvers = require('./resolvers');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+mongoose
+  .connect(process.env.MONGO_URI,
+    { useNewUrlParser: true})
+  .then(() => console.log('db connected'))
+  .catch(err => console.log(err));
 
 const server = new ApolloServer({
   typeDefs,
@@ -10,4 +18,4 @@ const server = new ApolloServer({
 
 server.listen().then(({ url }) => {
   console.log(`Server listening on ${url}`)
-})
+});
