d00961f93fa4b8838073da6df5c479c2a7d95d0c
Dejan <dejan.belic@develomon.com>
2019-04-14 13:18:07 +0200

    Added google login button. l9

--- client/src/components/Auth/Login.js
+++ client/src/components/Auth/Login.js
@@ -1,9 +1,20 @@
 import React from "react";
-import { withStyles } from "@material-ui/core/styles";
+import {withStyles} from "@material-ui/core/styles";
 // import Typography from "@material-ui/core/Typography";
+import {GoogleLogin} from "react-google-login";
 
-const Login = ({ classes }) => {
-  return <div>Login</div>;
+const Login = ({classes}) => {
+  const onSuccess = googleUser => {
+    const idToken = googleUser.getAuthResponse()
+      .id_token;
+    console.log(idToken)
+  };
+
+  return <GoogleLogin
+    clientId={"924711764819-50aliotmo4ohkrd76b1h737gmrgf4m0e.apps.googleusercontent.com"}
+    onSuccess={onSuccess}
+    isSignedIn={true}
+  />;
 };
 
 const styles = {
